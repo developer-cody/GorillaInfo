852b8068059cee5a2646b3f7a6ec3f6ecadf75e8
Cody <rodericgickle@gmail.com>
2025-02-19 20:04:01 -0800

    Add files via upload

---
+++ GorillaInfo/GorillaInfoPlugin.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace GorillaInfo
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class GorillaInfoPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.SillyCody.GorillaInfo";
+        private const string PluginName = "GorillaInfo";
+        private const string VersionString = "2.0.0";
+
+        private GameObject infoTextObject;
+        private TextMeshPro infoText;
+
+        private static string RoomInfo()
+        {
+            return PhotonNetwork.InRoom
+                ? $"Room: {PhotonNetwork.CurrentRoom.Name} | Players: {PhotonNetwork.PlayerList.Length}/10"
+                : "Not in room";
+        }
+
+        private static string PerformanceInfo()
+        {
+            float fps = Mathf.Ceil(1.0f / Time.unscaledDeltaTime);
+            return $"FPS: {fps} | Ping: {PhotonNetwork.GetPing()}";
+        }
+
+        public static string Text()
+        {
+            return $"{RoomInfo()}\n{PerformanceInfo()}\n";
+        }
+
+        private void Start() => GorillaTagger.OnPlayerSpawned(InitTextObjects);
+
+        private void InitTextObjects()
+        {
+            if (infoTextObject == null)
+            {
+                infoTextObject = new GameObject("GorillaInfoText");
+                infoText = infoTextObject.AddComponent<TextMeshPro>();
+
+                infoText.font = GorillaTagger.Instance.offlineVRRig.playerText1.font;
+                infoText.fontSize = 0.35f;
+                infoText.alignment = TextAlignmentOptions.Center;
+                infoText.color = Color.white;
+                infoText.isOverlay = true;
+
+                Transform cam = Camera.main.transform;
+                infoTextObject.transform.position = cam.position + cam.forward * 1.0f - cam.up * 0.4f;
+                infoTextObject.transform.SetParent(cam);
+                infoTextObject.layer = 19;
+            }
+        }
+
+        private void Update()
+        {
+            infoText.text = Text();
+
+            Transform cam = Camera.main.transform;
+            infoTextObject.transform.LookAt(cam);
+            infoTextObject.transform.Rotate(0, 180, 0);
+        }
+    }
+}
---
+++ GorillaInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GorillaInfo")]
+[assembly: AssemblyDescription("GorillaInfo is a developer-focused mod for Gorilla Tag, designed to be Harmony-patched for customizable text displays. It shows vital information at the bottom of the screen, allowing developers to tailor the data shown for debugging and development purposes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GorillaInfo")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6cda6ab-dcde-4c7f-aa08-a172dc74e324")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
